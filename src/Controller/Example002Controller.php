523316218ec7b35112832808e611254f63b19b63
astems_shhong <seonghun.hong@astems.co.kr>
2018-10-24 14:14:33 +0900

    use Annotation Routes

---
+++ src/Controller/Example002Controller.php
@@ -0,0 +1,35 @@
+<?php
+
+
+
+namespace App\Controller;
+
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+use Symfony\Component\HttpFoundation\Response;
+
+
+
+class Example002Controller
+
+
+{
+
+    /**
+
+     * @Route("/")
+
+     */
+
+    public function homepage()
+
+
+    {
+
+        return new Response('심포니4 Annotation Routes 이용');
+
+    }
+
+}

829a147b2a1c62ce7f8375a235fdf674f26d7004
astems_shhong <seonghun.hong@astems.co.kr>
2018-10-25 11:48:11 +0900

    Example004Controller.php

---
+++ src/Controller/Example004Controller.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class Example004Controller extends AbstractController
+{
+  /**
+   * @Route("/")
+   */
+    public function homepage()
+    {
+    return new Response('OMG! My first page already! WOOO!');
+    }
+
+    public function show($slug)
+    {
+        return $this->render('temppage/show.html.twig', [
+        'subject' => ucwords(str_replace('/',' ',$slug)),
+        ]);
+    }
+
+}
